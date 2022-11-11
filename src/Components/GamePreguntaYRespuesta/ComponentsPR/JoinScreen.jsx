8d44d275b8c669991350a6f3a8f893366f610ff2
MatiasGrrrOrtega <mat-1998-ort@hotmail.com>
2022-11-10 21:56:23 -0300

    pruebas con el juego de preguntas y respuestas

---
+++ src/Components/GamePreguntaYRespuesta/ComponentsPR/JoinScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function JoinScreen({start}) {
+    return (
+        <div className='join-screen'>
+            <h2>Join Quiz</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button onClick={start}>Start</button>
+        </div>
+    )
+}
