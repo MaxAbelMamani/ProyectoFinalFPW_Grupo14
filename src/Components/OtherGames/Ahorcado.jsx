3ebdfaa8e4947965843d0a87434095bd07d7ef92
JujenioP <110836213+MaxAbelMamani@users.noreply.github.com>
2022-11-13 20:34:34 -0300

    Subiendo juego Ahorcado

--- src/Components/OtherGames/Ahorcado.jsx
+++ src/Components/OtherGames/Ahorcado.jsx
@@ -6,7 +6,7 @@ export default function Ahorcado() {
     <div className='container__game'>
         <h1 className='title'>Ahorcado</h1>
         <div className='game'>
-          {/* <iframe src="#" frameBorder="0"></iframe> */}
+           <iframe src="https://ahorcado-grupo14-gp5pn2wi3-maxabelmamani.vercel.app/" frameBorder="0" width="1100" height="600"></iframe> 
         </div>
         <Link className='btn' to={"/"}><span>Inicio</span></Link>
         <Link className='btn' to={"/MenuGames"}><span>Menu Juegos</span></Link>
