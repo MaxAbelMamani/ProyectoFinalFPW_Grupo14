d4920fe202c57b2fd58bca819132c8725d39ded8
MatiasGrrrOrtega <mat-1998-ort@hotmail.com>
2022-11-13 23:10:56 -0300

    pequeños cambios

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../assets/img/logo.png'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
         <header className='header'>
           <h1 className='title__header'>Grupo 14</h1>
         </header>
+        <div className='logo__home'>
+          <img src={logo} alt="logo"/>
+        </div>
         <Link className='btn' to={"/MenuGames"}><span>Juegos</span></Link> 
         <Link className='btn' to={"/Team"}><span>Equipo</span></Link> 
     </div>
